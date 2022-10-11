fbe425671fec7cd955c88367e22145a93fd89b60
LupoArticoblu <wolf94-@libero.it>
2022-10-11 21:33:08 +0200

    End

--- script.js
+++ script.js
@@ -5,18 +5,21 @@ const trattaPercorsa = parseInt(prompt('Digitare la destinazione, in km da perco
 const bigliettoKM    = 0.21;
 
 const costoBiglietto = bigliettoKM * trattaPercorsa;
-const costoBig20 = (bigliettoKM * trattaPercorsa) / 0.02;
-const costoBig40 = (bigliettoKM * trattaPercorsa) / 0.04;
+
+const costoBig20 = costoBiglietto - (costoBiglietto * 0.2);
+
+const costoBig40 =
+costoBiglietto - (costoBiglietto * 0.4);
 
 console.log('costo del biglietto senza sconti', costoBiglietto);
 
 if(etaPasseggero < 18){
   console.log('utente20%');
-  document.getElementById('biglietto').innerHTML +=` Buon viaggio ${nomePasseggero}, hai diritto ad uno sconto del 20%. Prezzo finale: ${costoBig20}€ 
+  document.getElementById('biglietto').innerHTML +=` Buon viaggio ${nomePasseggero}, hai diritto ad uno sconto del 20%. Prezzo finale: ${costoBig20.toFixed(2)}€ 
   grazie e arrivederci.`
 }else if(etaPasseggero >= 65){
   console.log('utente40%');
-  document.getElementById('biglietto').innerHTML +=` Buon viaggio ${nomePasseggero}, hai diritto ad uno sconto del 40%. Prezzo finale: ${costoBig40}€ grazie e arrivederci`
+  document.getElementById('biglietto').innerHTML +=` Buon viaggio ${nomePasseggero}, hai diritto ad uno sconto del 40%. Prezzo finale: ${costoBig40.toFixed(2)}€ grazie e arrivederci`
 }else{
   console.log('utenteNaN%');
   document.getElementById('biglietto').innerHTML +=` Buon viaggio ${nomePasseggero}, non ci sono sconti disponibili per te. Prezzo finale: ${costoBiglietto}€ `
