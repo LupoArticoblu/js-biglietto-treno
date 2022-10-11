783e6a43de5a30042303bbb0768f3efd480b0902
Lorenzo C <wolf94-@libero.it>
2022-10-11 16:44:08 +0200

    inizio

---
+++ script.js
@@ -0,0 +1,9 @@
+//Inserire le varie proprietà come variabili
+
+const etaPasseggero = prompt(parseInt('Digitare l\'età del passeggero, in anni compiuti'));
+
+
+const trattaPercorsa = prompt(parseInt('Digitare la destinazione, in km da percorrere'));
+
+const bigliettoKM = 0.21;
+
